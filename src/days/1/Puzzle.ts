18ecbdf04db0db3779271afe6825781f72674bc5
Nitish <tnitish654@gmail.com>
2024-12-01 16:42:48 +0800

    🎄 Day 1 done

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,15 +1,51 @@
 const first = (input: string) => {
-  console.log(input);
-  return 'solution 1';
-};
+	const inputLines = input.split("\n")
+	const LList: number[] = []
+	const RList: number[] = []
 
-const expectedFirstSolution = 'solution 1';
+	inputLines.forEach((line) => {
+		const numbers = line.split("   ")
+		LList.push(parseInt(numbers[0]))
+		RList.push(parseInt(numbers[1]))
+	})
+
+	LList.sort((a, b) => a - b)
+	RList.sort((a, b) => a - b)
+
+	const totalDistance = LList.reduce((prev, curr, i) => {
+		const diff = Math.abs(curr - RList[i])
+		return prev + diff
+	}, 0)
+
+	return totalDistance
+}
+
+const expectedFirstSolution = 11
 
 const second = (input: string) => {
-  console.log(input);
-  return 'solution 2';
-};
+	const inputLines = input.split("\n")
+	const LList: number[] = []
+	const RList: number[] = []
 
-const expectedSecondSolution = 'solution 2';
+	inputLines.forEach((line) => {
+		const numbers = line.split("   ")
+		LList.push(parseInt(numbers[0]))
+		RList.push(parseInt(numbers[1]))
+	})
 
-export { first, expectedFirstSolution, second, expectedSecondSolution };
+	const counter: Record<string, number> = {}
+
+	const similarityScore = LList.reduce((acc, number, i) => {
+		if (!counter[`${number}`]) {
+			counter[`${number}`] = number * RList.filter((RListNum) => RListNum === number).length
+		}
+
+		return acc + counter[`${number}`]
+	}, 0)
+
+	return similarityScore
+}
+
+const expectedSecondSolution = 31
+
+export { first, expectedFirstSolution, second, expectedSecondSolution }
