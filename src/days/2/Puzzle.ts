676f7b862d83689ea9a95a02e86e00601bf6b83a
Nitish <tnitish654@gmail.com>
2024-12-07 14:15:00 +0800

    🎄 Day 2 done

--- src/days/2/Puzzle.ts
+++ src/days/2/Puzzle.ts
@@ -1,13 +1,74 @@
 const first = (input: string) => {
-  return 'solution 1';
-};
+	const reports = input.split("\n").map((line) => line.split(" ").map((item) => parseInt(item)))
 
-const expectedFirstSolution = 'solution 1';
+	const safeLayers: number[][] = []
+
+	reportLoop: for (let report of reports) {
+		let isIncreasing = false
+
+		for (let i = 0; i < report.length - 1; i++) {
+			const diff = report[i] - report[i + 1]
+
+			if (i === 0) {
+				isIncreasing = diff > 0
+			}
+
+			if (diff > 3 || diff < -3 || diff === 0) continue reportLoop
+			if (i !== 0 && isIncreasing !== diff > 0) continue reportLoop
+		}
+
+		safeLayers.push(report)
+	}
+
+	return safeLayers.length
+}
+
+const expectedFirstSolution = 2
 
 const second = (input: string) => {
-  return 'solution 2';
-};
+	const reports = input.split("\n").map((line) => line.split(" ").map((item) => parseInt(item)))
 
-const expectedSecondSolution = 'solution 2';
+	const safeLayers: number[][] = []
 
-export { first, expectedFirstSolution, second, expectedSecondSolution };
+	const checkSafe = (report: number[]) => {
+		let isIncreasing = false
+
+		for (let i = 0; i < report.length - 1; i++) {
+			const diff = report[i] - report[i + 1]
+
+			if (i === 0) {
+				isIncreasing = diff > 0
+			}
+
+			if (diff > 3 || diff < -3 || diff === 0) return false
+			if (i !== 0 && isIncreasing !== diff > 0) return false
+		}
+
+		return true
+	}
+
+	reportLoop: for (let report of reports) {
+		const isSafe = checkSafe(report)
+
+		if (!isSafe) {
+			for (let i = 0; i < report.length; i++) {
+				const isRemovedSafe = checkSafe(report.toSpliced(i, 1))
+
+				if (isRemovedSafe) {
+					safeLayers.push(report)
+					continue reportLoop
+				}
+			}
+		}
+
+		if (isSafe) {
+			safeLayers.push(report)
+		}
+	}
+
+	return safeLayers.length
+}
+
+const expectedSecondSolution = 4
+
+export { first, expectedFirstSolution, second, expectedSecondSolution }
